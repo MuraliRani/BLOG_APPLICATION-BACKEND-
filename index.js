92e6aac7560ea777789d403951c88b7ab3325f1a
Murali R <muralideva443@gmail.com>
2024-08-18 17:07:56 +0530

    completed

--- index.js
+++ index.js
@@ -76,10 +76,13 @@ app.post("/login", async (req, res) => {
 app.get("/profile", (req, res) => {
   const { token } = req.cookies;
   jwt.verify(token, secret, {}, (err, info) => {
-    if (err) throw err;
+    if (err) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
     res.json(info);
   });
 });
+
 
 app.post("/logout", (req, res) => {
   res.cookie("token", "").json("ok");
